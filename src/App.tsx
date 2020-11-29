50610362f82d3186d4a0afe9fd6eb02a2e8376cd
Alberto Tuit <albertotuitjara@gmail.com>
2020-11-29 19:06:42 +0100

    Created about me section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import AboutMe from './pages/AboutMe';
 
 function App() {
     return (
         <div>
-            <h1>asida</h1>
+            <AboutMe />
         </div>
     );
 }
