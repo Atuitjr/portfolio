2b16ee3e8a93dc89b89e2de39c552050a6845f3a
Alberto Tuit <albertotuitjara@gmail.com>
2020-11-29 19:26:38 +0100

    FAQ Section

---
+++ src/components/FaqSection.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const FaqSection = () => {
+    return (
+        <div className='faq'>
+            <h2>
+                Any questions <span>FAQ</span>
+            </h2>
+            <div className='question'>
+                <h4>how do i start?</h4>
+                <div className='answer'>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit.
+                    </p>
+                </div>
+            </div>
+            <div className='question'>
+                <h4>Daily schedule.</h4>
+                <div className='answer'>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit.
+                    </p>
+                </div>
+            </div>
+            <div className='question'>
+                <h4>What technologies do i use.</h4>
+                <div className='answer'>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit.
+                    </p>
+                </div>
+            </div>
+            <div className='question'>
+                <h4>What can i offer.</h4>
+                <div className='answer'>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FaqSection;
--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
 import ProjectsSection from '../components/ProjectsSection';
+import FaqSection from '../components/FaqSection';
 
 const AboutMe = () => {
     return (
         <>
             <AboutSection />
-            <ProjectsSection />{' '}
+            <ProjectsSection />
+            <FaqSection />
         </>
     );
 };
