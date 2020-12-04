c595238f2884a00f79671b50a3329c11687e4983
Alberto Tuit <albertotuitjara@gmail.com>
2020-12-04 19:01:08 +0100

    Added react routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import AboutMe from './pages/AboutMe';
+import Projects from './pages/Projects';
+import ContactMe from './pages/ContactMe';
 import GlobalStyle from './components/GlobalStyle';
 import Nav from './components/Nav';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
     return (
         <div>
             <GlobalStyle />
             <Nav />
-            <AboutMe />
+            <Switch>
+                <Route path='/' exact>
+                    <AboutMe />
+                </Route>
+                <Route path='/projects'>
+                    <Projects />
+                </Route>
+                <Route path='/contact'>
+                    <ContactMe />
+                </Route>
+            </Switch>
         </div>
     );
 }
