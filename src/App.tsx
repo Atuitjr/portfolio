4b2a0e0de9fb109bf953474e8a616a825971dcef
Alberto Tuit <albertotuitjara@gmail.com>
2020-12-04 18:16:03 +0100

    Created nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AboutMe from './pages/AboutMe';
 import GlobalStyle from './components/GlobalStyle';
+import Nav from './components/Nav';
 
 function App() {
     return (
         <div>
             <GlobalStyle />
+            <Nav />
             <AboutMe />
         </div>
     );
